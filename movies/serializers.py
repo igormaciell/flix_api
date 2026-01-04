2bbd708e6c6b31bea7ae1ed053cc37c5755bb652
Igor Santos Maciel <igorsantosmaciell@gmail.com>
2026-01-03 23:10:49 -0300

    Adicionando dados calculados de média das avaliações dos filmes.

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from .models import Movie
 
@@ -10,14 +11,16 @@ class MovieSerializer(serializers.ModelSerializer):
             fields = '__all__'
 
       def get_rate(self, obj):
-            ...
+            rate = obj.reviews.aggregate(Avg("stars"))["stars__avg"]
+
+            return round(rate, 1) if rate is not None else None
 
       def validate_release_date(self, value):
-            if value.year < 1990:
+            if value.year < 1900:
                   raise serializers.ValidationError("A data de lançamento não pode ser anterior a 1990.")
             return value
       
       def validate_resume(self, value):
-            if len(value) > 200:
+            if len(value) > 500:
                   raise serializers.ValidationError("Resumo não deve ter mais de 200 caracteres.")
             return value
