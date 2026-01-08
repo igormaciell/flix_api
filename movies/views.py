4e9406f60fd445c9824b83cd1b62fdb36dacbe20
Igor Santos Maciel <igorsantosmaciell@gmail.com>
2026-01-07 23:33:44 -0300

    Implementação classe global de permissões

--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,16 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
 from .models import Movie
 from .serializers import MovieSerializer
 
 
 class MovieCreateListView(generics.ListCreateAPIView):
-      permission_classes = [IsAuthenticated, ]
+      permission_classes = [IsAuthenticated, GlobalDefaultPermission]
       queryset = Movie.objects.all()
       serializer_class = MovieSerializer
 
 class MovieRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-      permission_classes = [IsAuthenticated, ]    
+      permission_classes = [IsAuthenticated, GlobalDefaultPermission]    
       queryset = Movie.objects.all()
       serializer_class = MovieSerializer
